e31307c5bb0982f34739598a475e0b1afae0aaab
Saumya Jain <samjain0913@gmail.com>
2020-06-24 15:44:24 +0530

    init

---
+++ app/src/main/java/com/oblivion/videocall/MainActivity.java
@@ -0,0 +1,15 @@
+package com.oblivion.videocall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
